5d86f02d893ef5714a859e88f3662fb7770bb7cc
Odilbek Group <111681616+OdilbekGroup@users.noreply.github.com>
2021-08-26 14:45:51 +0500

    if-else operatori so'ngi amaliyotlar
    if-else operatori mavzusidagi so'ngi amaliyotlar joylandi.

--- 5-kun/4.2 if-else operatori/demo.py
+++ 5-kun/4.2 if-else operatori/demo.py
@@ -1,4 +1,4 @@
-# 4.2 if-else operatori
+# # 4.2 if-else operatori
 
 son = int(input("Biror son kiriting.\n>>>"))
 if son > 0:
@@ -20,8 +20,8 @@ if yosh > 65: print('Siz covid-10 risk guruhida ekkansiz.')
 x, y = 25, 50
 print('x>y') if x>y else print('x<y')
 
-# Amaliyot
-#1.!
+# # Amaliyot
+# #1.!
 avtolar = ['audi', 'bmw', 'volvo', 'kia', 'hyundai']
 for avto in avtolar:
     if avto == 'bmw':
@@ -29,28 +29,28 @@ for avto in avtolar:
     else:
         print(avto.title())
 
-#1.2
+# #1.2
 javob = float(input("12x6 necha bo'ladi?\n>>>"))
 if javob!=72:
     print("Javob noto'g'ri")
 else:
     print("Javob to'g'ri")
 
-#1.3
+# #1.3
 ypsh = int(input('Yoshingiz nechada?\n>>>'))
 if ypsh>18:
     print('Xush kelibsiz!')
 else:
     print('Kirish mumkin emas.')
 
-#1.4
+# #1.4
 yil = int(input("Nechanchi yil tug'ilgansiz?\n>>>"))
 if 2022 - yil<18:
     print(f"Yoshingiz {2022-yil} da ekan.")
     print('kirish mumkin emas.')
 else:
     print("Xush kelibsiz!")
-#1.5
+# #1.5
 login = (input("Login kiriting."))
 if len(login)<=5:
     print("Login kamida 5 ta belgidan iborat bo'lishi kerak.")
@@ -58,7 +58,7 @@ else:
     print("Login qabul qilindi.")
 
 
-#2.1
+# #2.1
 cars = ['Tayota', "mazda", 'hyundai', 'gm', 'kia']
 for car in cars:
     if car=="gm":
@@ -66,7 +66,7 @@ for car in cars:
     else:
         print(car.title())
 
-#3
+# #3
 cars = ['Tayota', "mazda", 'hyundai', 'gm', 'kia']
 for car in cars:
     if car!="gm":
@@ -74,7 +74,7 @@ for car in cars:
     else:
         print(car.title())
 
-#4
+# #4
 login = input("Loginingiz kiriting.")
 if login.lower() == "admin":
     print("Xush kelibsiz, admin! "
@@ -82,7 +82,7 @@ if login.lower() == "admin":
 else:
     print(f'Xush kelibsiz, {login}')
 
-#5
+# #5
 print('Ikki ta son kiriting.')
 son1 = int(input("birinchi son\n>>>"))
 son2 = int(input("Ikkinchi son\n>>>"))
@@ -91,10 +91,24 @@ if son1 == son2:
 else:
     print(f"{son1} va {son2} teng emas")
 
-#6
+# #6
 son = int(input("Ixtiyoriy son kiriting."))
 if son > 0:
     print(f"{son} musbat son")
 else:
     print(f"{son} manfiy son")
 
+
+
+son = int(input("Musbat son kiriting."))
+if son >0:
+    print(f"{son} ning ildizi {int(son**(1/2))}")
+else:
+    print(f"Iltimos, musbat son kiriting.")
+
+inputNumber = int(input('Biror sonni kiriting.'))
+juftTow_son = inputNumber % 2
+if juftTow_son==0:
+    print(f"{inputNumber} - juft son")
+else:
+    print(f"{inputNumber} - toq son")
